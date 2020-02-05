412ac1c36fb8fc5b2fb416b903bf4163d9253722
Veronica Isabel Herrera <47429735+veroisabelherrera@users.noreply.github.com>
2020-02-05 13:03:57 -0300

    creación de cuenta e inicio de sesión funcionandocon firebase (#1)

---
+++ src/firebase-config.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyCLA_dSSlsWUmaV6w7nJil8RiwdM2AD5uQ",
+    authDomain: "laborum-scl011.firebaseapp.com",
+    databaseURL: "https://laborum-scl011.firebaseio.com",
+    projectId: "laborum-scl011",
+    storageBucket: "laborum-scl011.appspot.com",
+    messagingSenderId: "116883762326",
+    appId: "1:116883762326:web:f4c9872febdee43da366b7",
+    measurementId: "G-MT17B4HRXW"
+  }
