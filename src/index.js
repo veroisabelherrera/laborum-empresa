f0879a19f826c5bef0ed3575d338dd6b201b76ad
Veronica Isabel <veronica@Veronica-Isabel.local>
2020-02-05 12:55:17 -0300

    creación de cuenta e inicio de sesión funcionandocon firebase

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import firebaseConfig from './firebase-config';
+
+import { FirebaseAppProvider } from 'reactfire'
+
+ReactDOM.render((
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+        <Suspense fallback={'Conectando a la app'}>
+            <App />
+        </Suspense>
+    </FirebaseAppProvider>
+), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
