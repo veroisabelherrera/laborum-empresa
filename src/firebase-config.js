1ca6f16d5c188042352927b861ead274372c5bd5
Veronica Isabel <veronica@Veronica-Isabel.local>
2020-02-11 12:39:03 -0300

    alineando funcion de exportacion de firebase con irene

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,10 +1,15 @@
-export default {
-    apiKey: "AIzaSyCLA_dSSlsWUmaV6w7nJil8RiwdM2AD5uQ",
-    authDomain: "laborum-scl011.firebaseapp.com",
-    databaseURL: "https://laborum-scl011.firebaseio.com",
-    projectId: "laborum-scl011",
-    storageBucket: "laborum-scl011.appspot.com",
-    messagingSenderId: "116883762326",
-    appId: "1:116883762326:web:f4c9872febdee43da366b7",
-    measurementId: "G-MT17B4HRXW"
-  }
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/database';
+firebase.initializeApp({
+  apiKey: 'AIzaSyCLA_dSSlsWUmaV6w7nJil8RiwdM2AD5uQ',
+  authDomain: 'laborum-scl011.firebaseapp.com',
+  databaseURL: 'https://laborum-scl011.firebaseio.com',
+  projectId: 'laborum-scl011',
+  storageBucket: 'laborum-scl011.appspot.com',
+  messagingSenderId: '116883762326',
+  appId: '1:116883762326:web:f4c9872febdee43da366b7',
+  measurementId: 'G-MT17B4HRXW',
+});
+const db = firebase.firestore();
+export default db;
