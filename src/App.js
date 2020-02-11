7442c8c69efbae51cbc386c078b5b0d92de9271a
Veronica Isabel <veronica@Veronica-Isabel.local>
2020-02-11 12:23:58 -0300

    agregando estilos de pantalla login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './App.css';
-import Login from './assets/components/login/Login';
-import { useUser } from 'reactfire';
+import { LoginScreen } from './assets/components/login/LoginScreen';
 
 function App() {
-  const user = useUser();
 
   return (
     <div className="App">
-      { user && <p>Bienvenidx: {user.email} </p> }
-      <Login />
+      <LoginScreen />
     </div>
   );
 }
