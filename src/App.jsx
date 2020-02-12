fff2d424c46e4001330bdd6067188920d6433124
Cecilia Geraldo <ceciliandrea.geraldo@gmail.com>
2020-02-12 10:35:03 -0300

    Añadido archivo Home.js para realizar la vista luego de que el usuario inicia sesión

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import LoginScreen from './assets/components/login/LoginScreen';
+// import Sidebar from './assets/components/defaultComponents/Sidebar';
+
 
 function App() {
   return (
---
+++ src/assets/views/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Sidebar from '../components/defaultComponents/Sidebar';
+import Footer from '../components/defaultComponents/Footer';
+
+const Home = () => {
+
+};
+
+export default Home;
