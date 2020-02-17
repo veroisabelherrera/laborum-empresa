6f297b99164db50369a10ca37af890bcb79ba5fa
cynthiagomezmontoya <cgomezm066@gmail.com>
2020-02-17 18:01:24 -0400

    datos empresa

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Form from './componets/Forms';
 
 import firebaseConfig from './firebase-config';
 
@@ -12,8 +13,10 @@ ReactDOM.render((
     <FirebaseAppProvider firebaseConfig={firebaseConfig}>
         <Suspense fallback={'Conectando a la app'}>
             <App />
+            <Form />
         </Suspense>
     </FirebaseAppProvider>
+    
 ), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
