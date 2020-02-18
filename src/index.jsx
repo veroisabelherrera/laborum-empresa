277fbb5ea0e39bacf383c1e2ef0d0da6da060d65
Cecilia Geraldo <ceciliandrea.geraldo@gmail.com>
2020-02-18 10:38:47 -0300

    añadidos cambios desde el repo de Cynthia

--- src/index.jsx
+++ src/index.jsx
@@ -9,21 +9,11 @@ import Form from './componets/Forms';
 import firebaseConfig from './firebase-config';
 
 ReactDOM.render((
-<<<<<<< HEAD:src/index.jsx
   <FirebaseAppProvider firebaseConfig={firebaseConfig}>
     <Suspense fallback="Conectando a la app">
       <App />
     </Suspense>
   </FirebaseAppProvider>
-=======
-    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
-        <Suspense fallback={'Conectando a la app'}>
-            <App />
-            <Form />
-        </Suspense>
-    </FirebaseAppProvider>
-    
->>>>>>> a822cc426062c38c8c6db5f30188e1041819f6e7:src/index.js
 ), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
